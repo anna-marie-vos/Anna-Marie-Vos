a235cbc49f340a20fe288651489e629c1b2aba2b
anna-marie-vos <anna@manu.net.nz>
2019-06-02 16:43:38 +1200

    Header updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(window).on("scroll", function() {
+  if ($(this).scrollTop() > 100) {
+    $(".header p").remove();
+    $(".header").prepend(`<p>Anna-Marie Vos</p>`);
+  } else {
+    $(".header p").remove();
+    $(".header").prepend(`<p>Digital</p>`);
+  }
+});
